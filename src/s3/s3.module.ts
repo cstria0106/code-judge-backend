a7eeddb3dd5c7281c07c87af92fd5dd3d342f4ab
Lee Sangyeop <cstria0106@gmail.com>
2025-09-01 17:46:33 +0900

    add s3 endpoint / set docker repo url

--- src/s3/s3.module.ts
+++ src/s3/s3.module.ts
@@ -1,8 +1,7 @@
 import { S3Client } from '@aws-sdk/client-s3';
-import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { Global, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { AwsSdkModule, InjectAws, getClientToken } from 'aws-sdk-v3-nest';
+import { AwsSdkModule } from 'aws-sdk-v3-nest';
 
 @Global()
 @Module({
@@ -12,10 +11,12 @@ import { AwsSdkModule, InjectAws, getClientToken } from 'aws-sdk-v3-nest';
       clientType: S3Client,
       inject: [ConfigService],
       useFactory: async (config: ConfigService) => {
+        const endpoint = config.get<string>('AWS_ENDPOINT');
         const region = config.get<string>('AWS_REGION', 'ap-northeast-2');
         const id = config.getOrThrow<string>('AWS_ACCESS_KEY_ID');
         const secret = config.getOrThrow<string>('AWS_SECRET_ACCESS_KEY');
         return new S3Client({
+          endpoint,
           region,
           credentials: {
             accessKeyId: id,
